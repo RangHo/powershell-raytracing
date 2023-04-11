75b08c201994bd84d9ce211822007a39c4eb2ee3
RangHo Lee <hello@rangho.me>
2023-04-11 13:04:56 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Raytracing in a Weekend, PowerShell edition
+
+> This project is part of [Project Lemonade](https://rangho.dev/project-lemonade).
+
+Simple path tracer written in PowerShell.
---
+++ Source/PSRaytracing.psm1
@@ -0,0 +1,21 @@
+# Get list of class and function defintion files
+$classes = @(Get-ChildItem -Path $PSScriptRoot\Classes -Filter *.ps1 -Recurse -ErrorAction SilentlyContinue)
+$public = @(Get-ChildItem -Path $PSScriptRoot\Public -Filter *.ps1 -Recurse -ErrorAction SilentlyContinue)
+$private = @(Get-ChildItem -Path $PSScriptRoot\Private -Filter *.ps1 -Recurse -ErrorAction SilentlyContinue)
+
+# Source all files
+foreach ($file in ($public + $private))
+{
+    try
+    {
+        Write-Verbose "Importing $($file.FullName)"
+        . $file.FullName
+    }
+    catch
+    {
+        Write-Error -Message "Failed to import $($file.FullName): $($_.Exception.Message)"
+    }
+}
+
+# Export only public member functions
+Export-ModuleMember -Function $public.BaseName
